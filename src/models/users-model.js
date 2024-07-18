32b18552a13cca6c46ad467f373b695c6ac1c498
Gabriel Abal <129793663+gabriel-abal@users.noreply.github.com>
2024-07-18 18:12:34 -0300

    first commit

---
+++ src/models/users-model.js
@@ -0,0 +1,25 @@
+const uuid = require("uuid").v4;
+const bcrypt = require("bcrypt");
+
+const users = [
+  { id: "1", name: "Gabriel Abal", email: "gabriel@email", password: "123456" },
+  { id: "2", name: "John Snow", email: "john@email.com", password: "0000" },
+];
+
+module.exports = {
+  getAllUsers: () => users,
+
+  getUserById: (id) => users.find((user) => user.id === id),
+  getUserByEmail: (email) => users.find((user) => user.email === email),
+
+  createUser: (name, email, password) => {
+    const newUser = {
+      id: uuid(),
+      name,
+      email,
+      password: bcrypt.hashSync(password, 10),
+    };
+    users.push(newUser);
+    return newUser;
+  },
+};
